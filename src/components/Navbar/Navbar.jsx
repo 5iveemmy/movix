c0ca33716516f4a6dda80eef27a29bbb20dfe83d
5iveemmy <ainemmanuel87@gmail.com>
2023-01-13 22:22:21 +0100

    chore: create components for homepage

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Navbar.styles";
+
+const Navbar = () => {
+  return (
+    <S.Container>
+      <p>yo</p>
+    </S.Container>
+  );
+};
+
+export default Navbar;
---
+++ src/screens/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./Homepage.styles";
+const Homepage = () => {
+  return (
+    <S.Container>
+      <p>yoo</p>
+    </S.Container>
+  );
+};
+
+export default Homepage;
