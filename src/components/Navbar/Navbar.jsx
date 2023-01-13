548d54287a08d60d1577be7a2610282d0ce64690
5iveemmy <ainemmanuel87@gmail.com>
2023-01-13 23:29:11 +0100

    chore: create navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import * as S from "./Navbar.styles";
+import movixLogo from "../../assets/images/movixLogo.svg";
+import menu from "../../assets/images/menu.svg";
 
 const Navbar = () => {
   return (
     <S.Container>
-      <p>yo</p>
+      <S.Div>
+        <S.Image src={movixLogo} alt="movix logo" />
+        <S.Movix>Movix</S.Movix>
+      </S.Div>
+      <S.SearchContainer>
+        <S.Input type="text" placeholder="What do you want to watch?" />
+        <S.StyledSearchIcon />
+      </S.SearchContainer>
+      <S.Div>
+        <S.Name>Hi, $Fullname</S.Name>
+        <S.Image src={menu} alt="menu logo" />
+      </S.Div>
     </S.Container>
   );
 };
