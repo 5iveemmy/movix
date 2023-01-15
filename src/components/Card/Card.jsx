01ccae377aaea4265fda391addc5cca0a88806d1
5iveemmy <ainemmanuel87@gmail.com>
2023-01-16 00:46:13 +0100

    chore: display tv Series icon conditionally

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ import imdb from "../../assets/images/imdb.svg";
 import rottenTomatoes from "../../assets/images/rottenTomatoes.svg";
 import { useQuery } from "react-query";
 
-const Card = ({ title, img, rating, id }) => {
+const Card = ({ title, img, rating, id, seasons }) => {
   const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
   const MOVIE_DETAILS_URL =
@@ -20,7 +20,7 @@ const Card = ({ title, img, rating, id }) => {
   return (
     <S.Container>
       <S.Wrapper style={{ backgroundImage: `url('${IMG_URL + img}')` }}>
-        <S.Image src={tvSeries} alt="Tv series" top />
+        {seasons && <S.Image src={tvSeries} alt="Tv series" top />}
         <S.Image src={favorite} alt="Favourite" right />
       </S.Wrapper>
       <S.Description>
