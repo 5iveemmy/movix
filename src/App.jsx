5f823d524dc9bcc576150365d95b1598e01c1c3c
5iveemmy <ainemmanuel87@gmail.com>
2023-01-15 11:03:51 +0100

    chore: create privateRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "./auth/AuthContext";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "@firebase/auth";
 import { auth } from "./auth/Firebase";
+import { PrivateRoute } from "./PrivateRoute";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -24,7 +25,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Homepage />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Homepage />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
